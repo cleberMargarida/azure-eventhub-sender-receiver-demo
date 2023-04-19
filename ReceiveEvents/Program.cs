1818b480a6c08e8fa2f529edac85a14fc2ae9cf6
cleberMargarida <cleberfilho3@gmail.com>
2023-04-19 20:09:59 -0300

    Add project files.

---
+++ ReceiveEvents/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+
+const string connectionString = "Endpoint=sb://hackergamepass0401.servicebus.windows.net/;SharedAccessKeyName=sender;SharedAccessKey=R+P2ozg5E52qn2zypTBoEn658le0LM/qk+AEhCeY5e0=;EntityPath=test-event-hub";
+const string eventHubName = "test-event-hub";
+const string blobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=hackergamepass;AccountKey=Xii/PW4qITaYyDgOyzdWSIETKKSKTziRCZiuw6rwIsJAimvzCUZdCwdgqEVi0j6FlrDV0imlSg1K+AStvvJCIw==;EndpointSuffix=core.windows.net";
+const string blobContainerName = "test-blob";
+string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+
+// Create a blob container client that the event processor will use 
+BlobContainerClient storageClient = new BlobContainerClient(blobStorageConnectionString, blobContainerName);
+
+// Create an event processor client to process events in the event hub
+EventProcessorClient processor = new EventProcessorClient(storageClient, consumerGroup, connectionString, eventHubName);
+
+// Register handlers for processing events and handling errors
+processor.ProcessEventAsync += ProcessEventHandler;
+processor.ProcessErrorAsync += ProcessErrorHandler;
+
+// Start the processing
+await processor.StartProcessingAsync();
+
+// Wait for 10 seconds for the events to be processed
+await Task.Delay(TimeSpan.FromSeconds(1000));
+
+// Stop the processing
+async Task ProcessEventHandler(ProcessEventArgs eventArgs)
+{
+    // Write the body of the event to the console window
+    Console.WriteLine("\tRecevied event: {0}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
+
+    // Update checkpoint in the blob storage so that the app receives only new events the next time it's run
+    await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
+}
+
+Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+{
+    // Write details about the error to the console window
+    Console.WriteLine($"\tPartition '{eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
+    Console.WriteLine(eventArgs.Exception.Message);
+    return Task.CompletedTask;
+}
---
+++ SendEvents/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+
+const string connectionString = "Endpoint=sb://hackergamepass0401.servicebus.windows.net/;SharedAccessKeyName=sender;SharedAccessKey=R+P2ozg5E52qn2zypTBoEn658le0LM/qk+AEhCeY5e0=;EntityPath=test-event-hub";
+const string eventHubName = "test-event-hub";
+
+// Create a producer client that you can use to send events to an event hub
+await using var producerClient = new EventHubProducerClient(connectionString, eventHubName);
+
+foreach (var i in Enumerable.Range(0, 100))
+{
+    // Create a batch of events 
+    using var eventBatch = await producerClient.CreateBatchAsync();
+    // Add events to the batch. An event is a represented by a collection of bytes and metadata. 
+    eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Event: "+ i)));
+    // Use the producer client to send the batch of events to the event hub
+    await producerClient.SendAsync(eventBatch);
+    Console.WriteLine($"A batch of {i} events has been published.");
+    await Task.Delay(1000);
+}
